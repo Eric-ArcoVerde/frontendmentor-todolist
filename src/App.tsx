b8ceb6a19d6f3cc2e2b748bf542b3f24c819d0f6
Eric Barros Arco-Verde <eric.arcoverde@gmail.com>
2026-03-29 12:51:00 -0300

    feat: adicionar funcionalidade de adição de tarefas no TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
+import {useState, type FormEvent} from 'react'
 import TodoForm from "./components/TodoForm"
 import TodoHeader from "./components/TodoHeader"
 import TodoList from "./components/TodoList"
 import { TodoContainer } from "./components/TodoContainer"
 
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 function App() {
 
+  const [todoList, setTodoList] = useState<Todo[]>([])
+
+  const addTodo = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const todoItem = formData.get("todo") as string
+
+    console.log(todoItem);
+
+    if(!todoItem.trim()) return //verifica se o usuario colocou uma tarefa vazia
+
+    setTodoList(prev => [...prev, { //pega o array anterior e acrescenta a nova tarefa
+      id: Date.now(),
+      text: todoItem,
+      completed: false
+    }])
+  }
 
   return (
     <TodoContainer>
       <TodoHeader></TodoHeader>
 
-      <TodoForm></TodoForm>
+      <TodoForm addTodo={addTodo}></TodoForm>
 
       <TodoList></TodoList>
     </TodoContainer>
